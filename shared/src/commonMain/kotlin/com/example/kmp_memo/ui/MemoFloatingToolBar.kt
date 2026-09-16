9059295a9a1ba78f11f550a3bae1243f0e5acbe7
이정현 <cw03125@naver.com>
2026-09-16 13:40:52 +0900

    프리뷰 추가

--- shared/src/commonMain/kotlin/com/example/kmp_memo/ui/MemoFloatingToolBar.kt
+++ shared/src/commonMain/kotlin/com/example/kmp_memo/ui/MemoFloatingToolBar.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -120,5 +124,28 @@ fun PreviewMemoFloatingToolBarSelectedWrite() {
             selectedDestination = MemoDestination.WRITE,
             onDestinationClick = {},
         )
+    }
+}
+
+@Preview(
+    name = " InteractiveMode Floating toolbar",
+    showBackground = true,
+)
+@Composable
+private fun MemoFloatingToolBarPreview() {
+    var selectedDestination by remember {
+        mutableStateOf(MemoDestination.HOME)
+    }
+    MemoTheme {
+        Box(
+            modifier = Modifier.padding(24.dp),
+        ) {
+            MemoFloatingToolBar(
+                selectedDestination = selectedDestination,
+                onDestinationClick = { destination ->
+                    selectedDestination = destination
+                },
+            )
+        }
     }
 }
